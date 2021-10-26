822bbfca9f64cb29458b3bec539c203b974ad1ba
2DKun <88485614+2DKun@users.noreply.github.com>
2021-10-26 10:04:06 +0300

    3

---
+++ laba3/src/com/company/Pug.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public abstract class Dog {
+    private String name;
+    private int age;
+
+    public String getName() { return name; }
+    public int getAge() { return age; }
+
+    public Dog(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+    public abstract void displayInfo();
+}
